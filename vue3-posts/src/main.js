2f487745f9687288c7a2eb4cb363b2efead8c19e
김명욱 <anko@gsitm.com>
2023-01-22 21:58:42 +0900

    pinia setting

--- vue3-posts/src/main.js
+++ vue3-posts/src/main.js
@@ -1,24 +1,19 @@
 // main.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.js';
 import 'virtual:svg-icons-register';
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 import router from '@/routes';
-// import focus from '@/directives/focus';
 import globalDirectives from './plugins/global-directives';
 import dayjs from './plugins/dayjs';
-// import person from './plugins/person';
-// import globalComponents from './plugins/global-components';
 
 const app = createApp(App);
-// app.use(globalComponents);
-// app.use(person);
-// app.directive('focus', focus);
+const pinia = createPinia();
+app.use(router);
 app.use(globalDirectives);
 app.use(dayjs);
-app.use(router);
+app.use(pinia);
 app.mount('#app');
-
-// console.log(import.meta.env.VITE_APP_API_URL);
+import 'bootstrap/dist/js/bootstrap.js';
--- vue3-posts/src/views/AboutView.vue
+++ vue3-posts/src/views/AboutView.vue
@@ -2,13 +2,29 @@
 	<div>
 		<h2>About View</h2>
 		<p>{{ $route.path }}</p>
-		<p>{{ $route.name }}</p>
+		<!-- <p>{{ $route.name }}</p> -->
 		<button class="btn btn-primary" @click="$router.push('/')">
 			홈으로 이동
 		</button>
+		<h2>Stroe</h2>
+		<p>counter: {{ counter }}</p>
+		<p>doubleCount: {{ doubleCount }}</p>
+		<p>플러스: {{ doubleCountPlusOne }}</p>
+		<button @click="increment()">click!</button>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router';
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
+const route = useRoute();
+console.log('path', route.path);
+
+const store = useCounterStore();
+const { counter, doubleCount, doubleCountPlusOne } = storeToRefs(store);
+const { increment } = store;
+</script>
 
 <style lang="scss" scoped></style>
